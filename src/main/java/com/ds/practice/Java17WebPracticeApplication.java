b324c2afb15747b22dc66ad75fc667203ed43621
Deepthi Srinivas <91528983+deepthisjha@users.noreply.github.com>
2022-08-18 09:43:56 +0200

    initial code

---
+++ src/main/java/com/ds/practice/Java17WebPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ds.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17WebPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17WebPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/practice/Java17WebPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17WebPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
